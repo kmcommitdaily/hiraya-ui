0a4d2ce24ce164ec2ac2ce5a94b04a20c1c5891c
kimberly galos <kmgalos0909@gmail.com>
2024-04-30 02:34:44 +0800

    Feature: Dynamic  BC Icon

--- libs/hiraya/src/components/Button/index.tsx
+++ libs/hiraya/src/components/Button/index.tsx
@@ -1,25 +1,13 @@
 import { ButtonProps, ButtonStyled } from './style';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 const Button: React.FC<ButtonProps> = ({ ...props }) => {
-  if (props.buttonWidthSizes === 'iconOnly') {
-    return (
-      <ButtonStyled
-        variants={props.variants}
-        buttonHeightSizes={props.buttonHeightSizes}
-        buttonWidthSizes={props.buttonWidthSizes}
-      >
-        <FileDownloadIcon />
-      </ButtonStyled>
-    );
-  }
   return (
     <ButtonStyled
       variants={props.variants}
       buttonHeightSizes={props.buttonHeightSizes}
       buttonWidthSizes={props.buttonWidthSizes}
     >
-      {props.label}
+      {props.buttonWidthSizes === 'iconOnly' ? props.buttonIcon : props.label}
     </ButtonStyled>
   );
 };
