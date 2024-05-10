d2d3d453ed107991d4fb730a211471ad885b5ba0
kimberly galos <kmgalos0909@gmail.com>
2024-05-10 11:57:30 +0800

    ac-for review

--- libs/hiraya/src/components/Accordion/style.tsx
+++ libs/hiraya/src/components/Accordion/style.tsx
@@ -22,8 +22,9 @@ const AccordionStyled = styled(MUIAccordion)<AccordionProps>(
         display: 'block',
         width: '100%',
         height: '1px',
-        background: 'rgba(0, 0, 0, 0.12)', // Adjust the color as needed
+        background: 'rgba(0, 0, 0, 0.323)',
         marginTop: '0',
+        marginBottom: '5px',
       },
     },
   }),
@@ -31,15 +32,18 @@ const AccordionStyled = styled(MUIAccordion)<AccordionProps>(
 
 const AccordionSummaryStyled = styled(MUIAccordionSummary)(() => ({
   '&.MuiAccordionSummary-root': {
+    margin: 0,
     '&:hover': {
       backgroundColor: '#e0e7efc8',
     },
     '&:focus': {
-      boxShadow: '0 0 10px 3px #90A5BB',
+      boxShadow: '0 0 5px 3px #9aaae4',
       backgroundColor: '#e0e7efc8',
       borderRadius: '5px',
+      border: '1px solid white',
     },
   },
+
   '&.MuiAccordionSummary-content': {
     alignItems: 'center',
   },
